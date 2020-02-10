12209fc04028a38b099c905db7e1e4ce1cdd9c9a
llamosa <michael.llamosa@gmail.com>
2020-02-10 14:10:25 -0800

    -Updated app_name variable declaration

--- apps/code.py
+++ apps/code.py
@@ -3,10 +3,10 @@ import os
 import dash_core_components as dcc
 import dash_html_components as html
 
-if 'DYNO' in os.environ:
-    app_name = os.environ['DASH_APP_NAME']
-else:
-    app_name = 'deep weld'
+# if 'DYNO' in os.environ:
+#     app_name = os.environ['DASH_APP_NAME']
+# else:
+app_name = 'deepweld'
 
 layout = html.Div([
     dcc.Markdown(children=open('apps/main.py', 'r').read()),
