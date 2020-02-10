076effc69a4dedb300a62a4b4dcf53c09db8f187
llamosa <michael.llamosa@gmail.com>
2020-02-10 15:23:50 -0800

    -Updated app server call

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ import dash
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
+#server = app.server
 app.config.suppress_callback_exceptions = True
--- index.py
+++ index.py
@@ -27,6 +27,8 @@ def display_page(pathname):
         return code.layout
 
 
+server = app.server
+
 if __name__ == '__main__':
     app.run_server(debug=True)
 
